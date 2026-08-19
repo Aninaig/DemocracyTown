832136e75875a38291bf5ccc0aecd4abe0c8cebd
Aninaig <g.schomacker@icloud.com>
2026-08-19 14:06:32 +0200

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,26 @@
+
+
+function offen() {
+    document.getElementById("alt").style.display = "none";
+    document.getElementById("imp").style.display = "none";
+    document.getElementById("kleinX").style.display = "inline";
+    document.getElementById("antworten").style.display = "inline";
+}
+
+function zu() {
+    document.getElementById("alt").style.display = "inline";
+    document.getElementById("imp").style.display = "inline";
+    document.getElementById("kleinX").style.display = "none";
+    document.getElementById("antworten").style.display = "none";
+    document.getElementById("impressum").style.display = "none";
+}
+
+
+function offen2() {
+    document.getElementById("alt").style.display = "none";
+    document.getElementById("imp").style.display = "none";
+    document.getElementById("kleinX").style.display = "inline";
+    document.getElementById("impressum").style.display = "inline";
+}
+
+
